1f352e8617a1b6420b7186f3732b08d518188875
Jeni Dang <jenipdang@yahoo.com>
2022-01-14 10:49:55 -0800

    create app folder structure

---
+++ src/components/products/FilterByCategory.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FilterByCategory = () => {
+    return (
+        <div>
+            <h2>Filter by Category</h2>
+        </div>
+    )
+}
+
+export default FilterByCategory
---
+++ src/components/products/ProductContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductContainer = () => {
+    return (
+        <div>
+            <h2>Products Container</h2>
+        </div>
+    )
+}
+
+export default ProductContainer
