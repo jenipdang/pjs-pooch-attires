c61c3d4122983d744af8ef6c2478a5b274829bc1
Jeni Dang <jenipdang@yahoo.com>
2022-01-15 20:21:54 -0800

    add show more and show less button for the description

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
-import {Link} from 'react-router-dom'
-import '../css/Details.css'
+import { Link } from 'react-router-dom';
+import '../css/Details.css';
 
 const Product = ({ product, addCart }) => {
 	const { id, name, amount, description, images, care } = product;
@@ -10,36 +10,32 @@ const Product = ({ product, addCart }) => {
 
 	return (
 		<Card>
-			<div className='details' key={id}>
-				<img src={images.front} alt='item'/>
-				<div className='box'>
-					<div className='row'>
+			<div className="details" key={id}>
+				<div className='image' onClick={() => setIsFront(!isFront)}>
+				{isFront ? (
+					<img src={images.front} alt="item" />
+				) : (
+					<img src={images.back} alt="item" />
+				)}
+				</div>
+				<div className="box">
+					<div className="row">
 						<h2>{name}</h2>
 						<span>${amount}</span>
 					</div>
-					<p>{description}</p>
+					<p>
+						{readMore ? description : `${description.substring(0, 100)}...`}
+						<button className="btn" onClick={() => setReadMore(!readMore)}>
+							{readMore ? 'Show Less' : 'Read More'}
+						</button>
+					</p>
 					<p>{care}</p>
-					<Link to='/cart' className='cart' onClick={() => addCart(id)}>
+					<Link to="/cart" className="cart" onClick={() => addCart(id)}>
 						Add to cart
 					</Link>
 				</div>
 			</div>
 		</Card>
-		// <Card md={6} style={{ width: '18rem' }} onClick={() => setIsFront(!isFront)}>
-		// 	{isFront ? <Card.Img variant="top" src={images.front}/> : <Card.Img variant="top" src={images.back}/>}
-		// 	<Card.Body>
-		// 		<Card.Title>{name}</Card.Title>
-		// 		<Card.Text>{readMore ? description : `${description.substring(0, 100)}...`}
-		// 		<button className='btn' onClick={() => setReadMore(!readMore)}>
-		// 			{readMore ? 'Show Less' : 'Read More'}
-		// 		</button>
-		// 		</Card.Text>
-		// 		<Card.Text>{care}</Card.Text>
-		// 		<Card.Text style={{ color: 'darkred' }}>${amount}</Card.Text>
-		// 		<Button variant="dark w-100">Add to Cart</Button>
-		// 	</Card.Body>
-		// </Card>
-
 	);
 };
 
