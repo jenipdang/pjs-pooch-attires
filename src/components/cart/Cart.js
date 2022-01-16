611f5bc64a889dba82aa9cf0da6b3480cc8f0b6b
Jeni Dang <jenipdang@yahoo.com>
2022-01-15 18:02:33 -0800

    try to import mui but it broken the site. remove file

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,15 +1,11 @@
 import React from 'react'
-// import { Container, Typography, Button, Grid } from '@mui/material'
-// import { Link } from 'react-router-dom'
-
 
 const Cart = () => {
-
-    return (
-        <div>
-            <h2>Shoppig Cart</h2>
-        </div>
-    )
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default Cart
