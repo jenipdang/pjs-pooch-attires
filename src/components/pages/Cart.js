aaf05f5c193cffd7fafcf26f30640719f9de1b74
Jeni Dang <jenipdang@yahoo.com>
2022-01-17 13:30:31 -0800

    remove cart file test code

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -2,18 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Product from '../products/Product'
 
 const Cart = () => {
-  const [cart, setCart] = useState([])
-
-  useEffect(() => {
-    fetch('https://localhost:3001/products')
-    .then(r => r.json())
-    .then(data => {
-      const cartItem = data.filter((product) => !!product.cart)
-      setCart(cartItem)
-    })
-  }, [])
-
-  const addCart = cart.map(product => <Product key={product.id} product={product} />)
+ 
 
   return (
     <div>
