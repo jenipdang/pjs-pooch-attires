655e335f676a885021e2c0e927a040ec1bdfb7ba
Jeni Dang <jenipdang@yahoo.com>
2022-01-18 16:36:00 -0800

    completed about page with styling using boostrap and css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-    return (
-        <div>
-            <h2>About</h2>
-        </div>
-    )
-}
+	return (
+		<div className="container mt-4">
+			<h3 className="text-center">ABOUT US</h3>
+			<div className="underline"></div>
+			<div className="grid">
+				<div>
+					<h4>
+						<small className="text">OUR STORY</small>
+					</h4>
+					<p className="lead">
+						<strong>We love dogs</strong> - this is the prime reason why PJ's
+						Pooch Attires was created. In fact, we love them so much that we
+						want them to have their own clothing line! Here at PJ's Pooch
+						Attires, we understand that your pet is a part of the family because
+						we’re pet parents too. It is our pleasure to be able to share our
+						love of dogs with you. PJ's Pooch Attires isn't just a pet store. We
+						are pet people, pet lovers, and pet parents. We are just like you.
+						When you shop with us, you can feel confident knowing that we
+						wouldn’t sell anything that we wouldn’t give to our own pets.
+					</p>
+				</div>
+				<div>
+					<h4>
+						<small className="text">OUR MISSION</small>
+					</h4>
+					<p className="lead">
+						Our dogs deserve nothing but the best. We are uncompromising in our
+						search for the industry’s highest quality dog supplies and gifts. We
+						especially try to find unique dog products with a great sense of
+						style, safety, and durability. Our inventory represents the best the
+						industry has to offer, and we thoroughly research and test every
+						product we carry. We believe that your dog deserves the best. Rest
+						assured, if we wouldn’t give it to our dog, we won’t sell it to you!
+						Woof Apparel provides a broad range of cute and funny high quality
+						and pet-safe clothes available for all dog sizes - we don't want a
+						single dog to feel missed out! For every occasion and for daily
+						wear, we made sure that you and your dogs will have plenty to choose
+						from!
+					</p>
+				</div>
+				<div>
+					<h4>
+						<small className="text">OUR VISION</small>
+					</h4>
+					<p className="lead">
+						Here at PJ'S POOCH ATTIRES, we believe that EVERY dog deserves to
+						live a full and happy life - they deserve to look their best, too!
+						All dogs want to feel loved, and a happy dog makes a happy dog
+						parent! Our goal is to make fur babies and fur parents become even
+						closer and more connected through dog clothing. Let your dog wear a
+						piece of love from you wherever they go!
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default About
+export default About;
