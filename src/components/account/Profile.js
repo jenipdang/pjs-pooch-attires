b8966f1c4cbe6cf65c10b667d80da33b82855412
Jeni Dang <jenipdang@yahoo.com>
2022-01-24 23:35:34 -0800

    clean up account and sign in components

--- src/components/account/Profile.js
+++ src/components/account/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Profile = ({ users }) => {
+const Profile = () => {
 	let history = useNavigate();
 
 	const handleSignout = () => {
@@ -11,7 +11,7 @@ const Profile = ({ users }) => {
 		<div className="container">
 			<div className="text ">
 				<h2 style={{ textAlign: 'center', marginBottom: '1rem' }}>
-					Welcome {users.firstName}!
+					Welcome !!
 				</h2>
 			</div>
 			<input className='btn btn-dark m-4' type="button" value="Signout" onClick={handleSignout} />
